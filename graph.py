e130d2df9f935468dba2bd75741914196ec2ba92
Alex62-Creator <cham62ss@gmail.com>
2025-04-29 09:56:17 +0300

    update graf

--- graph.py
+++ graph.py
@@ -12,22 +12,43 @@ class Graph:
 
     # Метод для вывода граф
     def print_graph(self):
+        print("Структура графа:")
         for node in self.graph:
-            print(node, "->", " -> ".join(map(str, self.graph[node])))
+            #print(node, "->", " -> ".join(map(str, self.graph[node])))
+            print(f"Узел {node} связан с узлами {self.graph[node]}")
+
+    # Метод для вывода списка узлов графа
+    def print_node(self):
+        list_node = list(self.graph.keys())
+        for value in self.graph.values():
+            for node in value:
+                if node not in list_node:
+                    list_node.append(node)
+        print(f"Граф имеет следующие узлы {list_node}")
+
+    # Метод для вывода количества узлов графа
+    def print_count_node(self):
+        list_node = list(self.graph.keys())
+        for value in self.graph.values():
+            for node in value:
+                if node not in list_node:
+                    list_node.append(node)
+        print(f"Граф состоит из {len(list_node)} узлов")
 
     # Метод для вывода связей заданного узла
-    def print_node(self, node):
+    def print_one_node(self, node):
         if node not in self.graph:
             print(f"Узла {node} не существует")
         else:
-            return self.graph[node]
+            print(f"Узел {node} имеет связи с узлами {self.graph[node]}")
 
     # Метод для вывода количества связей заданного узла
     def get_count_edge(self, node):
         if node not in self.graph:
             print(f"Узла {node} не существует")
         else:
-            return len(self.graph[node])
+            #return len(self.graph[node])
+            print(f"Узел {node} имеет {len(self.graph[node])} соединений")
 
 # Создаем объект класса
 g = Graph()
@@ -43,10 +64,13 @@ g.add_edge(1, 4)
 g.add_edge(2, 3)
 g.add_edge(3, 4)
 
-print("Структура графа:")
+g.print_count_node()
+
+g.print_node()
+
 g.print_graph()
 
-print(f"Узел {1} имеет {g.get_count_edge(1)} соединения")
+g.get_count_edge(1)
 
-print(f"Узел {1} имеет связи с узлами {g.print_node(1)}")
+g.print_one_node(1)
 
